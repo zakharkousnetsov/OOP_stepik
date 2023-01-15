214f5493439856939e1852b0d9d743a9390248ae
etotrussky <kousnetsov@inbox.ru>
2023-01-15 22:52:44 +0300

    accepted solution

--- 1.5.5.py
+++ 1.5.5.py
@@ -5,24 +5,31 @@ class TriangleChecker:
         self.a = a
         self.b = b
         self.c = c
-
     def is_triangle(self):
-        for edge in [self.a, self.b, self.c]:
-            if not (isinstance(edge, float) or isinstance(edge, int) ) or isinstance(edge, bool) or (edge<=0):
-                return 1
 
+        for edge in [self.a, self.b, self.c]:
+            if not type(edge) in (int, float) or edge<=0:
+                return 1
         if (self.a+self.b)<self.c or (self.a+self.c)<self.b or (self.b+self.c)<self.a:
             return 2
-
         else:
             return 3
+
+    def is_traingle(self):
+        if not all(map(lambda x: type(x) in (int, float), (self.a, self.b, self.c))):
+            return 1
+        if not all(map(lambda x: x>0, (self.a, self.b, self.c))):
+            return 1
+        if (self.a+self.b)<self.c or (self.a+self.c)<self.b or (self.b+self.c)<self.a:
+            return 2
+        return 3
+
 
 a, b, c = map(int, input().split())
 
 tr = TriangleChecker(a, b, c)
 print(tr.is_triangle())
 
-a, b, c = map(int, input().split())
 
 # еще коммент
 # еще
